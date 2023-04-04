d2e07d84033afaa23d2f0dea9027a464e9ec1b75
Ken H <60443687+KenH2000@users.noreply.github.com>
2023-04-04 09:55:53 -0700

    Create assembly_6800.js

---
+++ assembly_6800.js
@@ -0,0 +1,9 @@
+
+;                (function() {
+                    window.require(["ace/snippets/assembly_6800"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
